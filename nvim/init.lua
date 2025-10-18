98efc9c1f3e8251a77fc5adf1b1f534932ab4ded
jobmos <jobmos95@gmail.com>
2025-10-19 12:44:40 +1300

    :q

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,10 @@ vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a T
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
+
+vim.opt.number = true         -- Shows absolute line number on current line
+vim.opt.relativenumber = true -- Shows relative line numbers on other lines
+
 vim.diagnostic.config({
   virtual_text = true,
 })
