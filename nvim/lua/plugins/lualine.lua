bd787a9875d128d4c58f2ac0b056c238aa6d470e
Job Mos <jobmos95@gmail.com>
2026-02-18 09:40:06 +1300

    Add git repo name function to lualine config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,9 +2,26 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+        local function git_repo_name()
+            local handle = io.popen('git rev-parse --show-toplevel 2>/dev/null')
+            if not handle then return '' end
+            local result = handle:read('*l')
+            handle:close()
+            if not result or result == '' then return '' end
+            return result:match('([^/]+)$')
+        end
+
         require('lualine').setup({
             options = {
                 theme = 'auto'
+            },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { git_repo_name, 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' },
             }
         })
     end,
