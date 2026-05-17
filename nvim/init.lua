733cc26ddfe9472dd523df93fc7817b3f880fb46
Job Mos <jobmos95@gmail.com>
2026-05-18 10:10:47 +1200

    Set indent with to 2 spaces for Lua files

--- nvim/init.lua
+++ nvim/init.lua
@@ -53,3 +53,13 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = "html",
 	command = "setlocal shiftwidth=2 tabstop=2"
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lua",
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
