4b537eb945cc528a52d05a5529adee3e3ee90597
Job Mos <jobmos95@gmail.com>
2026-05-18 10:33:22 +1200

    Revert gitsigns hunk navigation keymaps in gitsigns plugin conf

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,28 +1,8 @@
 return {
   'lewis6991/gitsigns.nvim',
   config = function()
-    local gs = require('gitsigns')
-    gs.setup({
+    require('gitsigns').setup({
       current_line_blame = false,
-      on_attach = function(bufnr)
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        map('n', ']c', function()
-          if vim.wo.diff then return ']c' end
-          vim.schedule(function() gs.next_hunk() end)
-          return '<Ignore>'
-        end, { expr = true })
-
-        map('n', '[c', function()
-          if vim.wo.diff then return '[c' end
-          vim.schedule(function() gs.prev_hunk() end)
-          return '<Ignore>'
-        end, { expr = true })
-      end
     })
   end
 }
